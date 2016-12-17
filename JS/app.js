f110b9b134692d31b00e12601c13990e46841f7d
zakattack9 <zakattack26111@gmail.com>
2016-12-16 14:24:30 -1000

    somewhat working

--- JS/app.js
+++ JS/app.js
@@ -2,14 +2,15 @@ console.log("JS is Linked Properly");
 
 function generateFortuneCookie(){
   var cookieText = document.getElementById("fortune-cookie-text");
-  var fortunesList = ["die 2mrrw", "find a dollar", "become poor"];
+  var fortunesList = ["die 2mrrw", "find a dollar", "become poor", "jump off a cliff", "turn into Batman"];
 
-  for (var i = 0; i <= 2; i++){
-  console.log("You will " + fortunesList[i] + ".")
-  
+  for (var i = 0; i <= 4; i++){
+  console.log("You will " + fortunesList[Math.floor(Math.random() * fortunesList.length)] + ".");
+  }
+  document.getElementById("cookieText").innerText;
+
   var node = document.createElement("LI");
   var textnode = document.createTextNode();
   node.appendChild(textnode);
   document.getElementById("previous-fortunes").appendChild(node);
-  }
 }
